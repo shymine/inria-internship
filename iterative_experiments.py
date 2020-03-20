99dd914b5afae9b57cb6a4c9473e7f856c53b112
shymine <timothee.neitthoffer@laposte.net>
2020-03-20 12:31:03 +0100

    update return for train model

--- iterative_experiments.py
+++ iterative_experiments.py
@@ -5,12 +5,13 @@ def train_model(models_path, device):
     model = arcs.create_resnet_iterative(models_path, return_name=False)
     print("Training iteratively...")
     print('model: {}'.format(model))
+    return model
 
 def main():
     random_seed = af.get_random_seed()
     models_path = 'networks/{}'.format(random_seed)
     device = af.get_pytorch_device()
-    model, dataset = train_model(models_path, device)
+    model = train_model(models_path, device)
 
 if __name__ == '__main__':
     main()
