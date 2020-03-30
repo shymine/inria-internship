086f54516e41af37e975e49af736c37be0634c2b
shymine <timothee.neitthoffer@laposte.net>
2020-03-30 10:22:57 +0200

    baseline experiment

--- iterative_experiments.py
+++ iterative_experiments.py
@@ -6,9 +6,8 @@ def train_model(models_path, device):
 
     iter_model, iter_params = arcs.create_resnet_iterative(models_path, 'iterative', return_name=False)
     full_ic_model, full_ic_params = arcs.create_resnet_iterative(models_path, 'full_ic', return_name=False)
-    # full_model, full_params = arcs.create_resnet_iterative(models_path, 'full', return_name=False)
-    # print("models iter, full_ic, full:\n{}\n{}\n{}".format(iter_model, full_ic_model, full_model))
-    print("full_ic: {}".format(full_ic_model))
+    full_model, full_params = arcs.create_resnet_iterative(models_path, 'full', return_name=False)
+
     print("Training...")
     dataset = af.get_dataset('cifar10', 128)
     lr = iter_params['learning_rate']/10 #0.01
@@ -21,36 +20,36 @@ def train_model(models_path, device):
 
     iter_name = iter_params['base_model']
     full_ic_name = full_ic_params['base_model']
-    # full_name = full_params['base_model']
+    full_name = full_params['base_model']
 
     opti_param = (lr, weight_decay, momentum, -1)
     lr_schedule_params = (milestones, gammas)
 
-    # iter_model.to(device)
+    iter_model.to(device)
     full_ic_model.to(device)
-    # full_model.to(device)
+    full_model.to(device)
 
     iter_name = iter_name + '_training'
     full_ic_name = full_ic_name + '_training'
-    # full_name = full_name + '_training'
+    full_name = full_name + '_training'
 
-    # iter_optimizer, iter_scheduler = af.get_full_optimizer(iter_model, opti_param, lr_schedule_params)
+    iter_optimizer, iter_scheduler = af.get_full_optimizer(iter_model, opti_param, lr_schedule_params)
     full_ic_optimizer, full_ic_scheduler = af.get_full_optimizer(full_ic_model, opti_param, lr_schedule_params)
-    # full_optimizer, full_scheduler = af.get_full_optimizer(full_model, opti_param, lr_schedule_params)
+    full_optimizer, full_scheduler = af.get_full_optimizer(full_model, opti_param, lr_schedule_params)
 
-    # iter_metrics = iter_model.train_func(iter_model, dataset, num_epochs, iter_optimizer, iter_scheduler, device)
+    iter_metrics = iter_model.train_func(iter_model, dataset, num_epochs, iter_optimizer, iter_scheduler, device)
     full_ic_metrics = full_ic_model.train_func(full_ic_model, dataset, num_epochs, full_ic_optimizer, full_ic_scheduler, device)
-    # full_metrics = full_model.train_func(full_model, dataset, num_epochs, full_optimizer, full_scheduler, device)
+    full_metrics = full_model.train_func(full_model, dataset, num_epochs, full_optimizer, full_scheduler, device)
 
-    # _link_metrics(iter_params, iter_metrics)
+    _link_metrics(iter_params, iter_metrics)
     _link_metrics(full_ic_params, full_ic_metrics)
-    # _link_metrics(full_params, full_metrics)
+    _link_metrics(full_params, full_metrics)
 
-    # arcs.save_model(iter_model, iter_params, models_path, iter_name, epoch=-1)
+    arcs.save_model(iter_model, iter_params, models_path, iter_name, epoch=-1)
     arcs.save_model(full_ic_model, full_ic_params, models_path, full_ic_name, epoch=-1)
-    # arcs.save_model(full_model, full_params, models_path, full_name, epoch=-1)
+    arcs.save_model(full_model, full_params, models_path, full_name, epoch=-1)
 
-    return iter_model#, full_ic_model, full_model
+    return (iter_model, iter_params), (full_ic_model, full_ic_params), (full_model, full_params)
 
 def _link_metrics(params, metrics):
     params['train_top1_acc'] = metrics['train_top1_acc']
@@ -64,7 +63,8 @@ def main():
     random_seed = af.get_random_seed()
     models_path = 'networks/{}'.format(random_seed)
     device = af.get_pytorch_device()
-    model = train_model(models_path, device)
+    iter, full_ic, full = train_model(models_path, device)
+    print("accuracies:\niter: {}, full_ic: {}, full: {}".format(iter[1]['test_top1_acc'][-1], full_ic[1]['test_top1_acc'][-1], full[1]['test_top1_acc'][-1]))
 
 if __name__ == '__main__':
     main()
