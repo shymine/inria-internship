20e382631b853d145d94c11995f4e4739166c793
shymine <timothee.neitthoffer@laposte.net>
2020-04-08 15:06:38 +0200

    correcting tensor is none problem 5

--- snip.py
+++ snip.py
@@ -104,12 +104,8 @@ def snip_skip_layers(model, keep_ratio, loader, loss, device='cpu'):
     grads_abs = []
     for layer in _model.modules():
         if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.Linear):
-            if layer.weight_mask.grad is None:
-                print("None")
-                print("layer: {}".format(layer))
-                print("weight_mask: {}".format(layer.weight_mask))
-                print("grad: {}".format(layer.weight_mask.grad))
-            grads_abs.append(torch.abs(layer.weight_mask.grad))
+            if layer.weight_mask.grad is not None:
+                grads_abs.append(torch.abs(layer.weight_mask.grad))
     all_scores = torch.cat([torch.flatten(x) for x in grads_abs])
     norm_factor = torch.sum(all_scores)
     all_scores.div_(norm_factor)
