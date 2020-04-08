358a06e6ca9bf89ac099bf06663b0b10dd3e0e58
shymine <timothee.neitthoffer@laposte.net>
2020-04-08 14:47:17 +0200

    correcting tensor is none problem 4

--- snip.py
+++ snip.py
@@ -104,7 +104,7 @@ def snip_skip_layers(model, keep_ratio, loader, loss, device='cpu'):
     grads_abs = []
     for layer in _model.modules():
         if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.Linear):
-            if layer.weight_mask.grad == None:
+            if not layer.weight_mask.grad:
                 print("None")
                 print("layer: {}".format(layer))
                 print("weight_mask: {}".format(layer.weight_mask))
