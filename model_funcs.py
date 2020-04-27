de5c951e2c70c3f17ff24c2a37b8b300acaa354a
shymine <timothee.neitthoffer@laposte.net>
2020-04-27 14:44:30 +0200

    test iter4 with grow on 0.01 diff

--- model_funcs.py
+++ model_funcs.py
@@ -668,7 +668,7 @@ def iter_training_4(model, data, epochs, optimizer, scheduler, device='cpu'):
         grow = False
         if len(acc_s[0])>=2:
             for a,b in zip(acc_s[0], acc_s[1]):
-                grow = abs(a-b)<0.001
+                grow = abs(a-b)<0.01
                 if not grow:
                     break
         return grow
